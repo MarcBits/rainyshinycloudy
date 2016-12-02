d78b678ed64b17247151457f3e21711b5dcb092f
MarcBits <marcbits@outlook.com>
2016-12-02 13:33:50 -0500

    Updated constants.

--- rainyshinycloudy/Constants.swift
+++ rainyshinycloudy/Constants.swift
@@ -12,7 +12,8 @@ let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
 let LATITUDE = "lat="
 let LONGITUDE = "&lon="
 let APP_ID = "&appid="
-let API_KEY = "76f00482b34f6e77aaa1c54476c8c0e7"
+
+let API_KEY = MY_API_KEY    // Define a key from OpenWeatherMap.org in constants file as "MY_API_KEY"
 
 typealias DownloadComplete = () -> ()
 
